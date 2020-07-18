ac12a1dd4e333f3f5a4b5aae0127e551fdbf0d22
unknown <shenkit108@gmail.com>
2020-07-18 18:10:10 +0800

    2. Endless Ground Spawning

---
+++ EndlessRunnerTutorial/Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour {
+
+    public GameObject groundTile;
+    Vector3 nextSpawnPoint;
+
+    public void SpawnTile ()
+    {
+        GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+        nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+    }
+
+    private void Start () {
+        for (int i = 0; i < 15; i++) {
+            SpawnTile();
+        }
+    }
+}
---
+++ EndlessRunnerTutorial/Assets/Scripts/GroundTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GroundTile : MonoBehaviour {
+
+    GroundSpawner groundSpawner;
+
+	private void Start () {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+	}
+
+    private void OnTriggerExit (Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+
+    private void Update () {
+	
+	}
+}
